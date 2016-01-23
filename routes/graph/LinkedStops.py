4237c65860e3a6bd0ca810b94c13a318fb1c8f9d
gabrieldemarmiesse <gabrieldemarmiesse@hotmail.fr>
2016-01-23 19:51:49 +0100

    Now we can read the line.txt file and store the lines to an object that is easier to use (StopOfGraph)

--- routes/graph/LinkedStops.py
+++ routes/graph/LinkedStops.py
@@ -1,4 +1,7 @@
-class LinkedStops(object):
+from routes.graph import StopOfGraph
+
+
+class LinkedStops():
     """This class is here to create a map of the stops in a line
 it will have a structure of graph so will be implement as a dictionary
 here is an example:
@@ -15,6 +18,8 @@ where the bus doesn't usually go
 
     def __init__(self):
         self.dictionary = dict()
+        self.line_id = "undefined"
+        self.create_from_file()
 
     def insert_node(self, main_node, *links_to_add):
         # The first argument is the node from where starts the links
@@ -34,3 +39,38 @@ where the bus doesn't usually go
         # the new links point to nodes which are not in the dictionary
         for node in links_to_add:
             self.insert_node(node)
+
+    def create_from_file(self, path="../../gtfs"):
+        stop_of_graph_list = self.read_file(path)
+
+    def read_file(self,path):
+        """this method parse the file line.txt to create a
+        list of StopOfGraph which is much easier to use to
+        create a graph"""
+
+        path += "/line.txt"
+        with open(path) as f:
+            lines = f.readlines()
+
+        stop_of_graph_list = list()
+        in_a_loop = False
+
+        # So here we're examining the lines of the file
+        for line in lines[1:]:
+            line = line.strip()
+
+            # If we encounter a *, it means that we enter a loop or leave one
+            if line == '*':
+                in_a_loop = not in_a_loop
+            elif line == '':
+                continue
+            else:
+                stop_of_graph_list.append(StopOfGraph.StopOfGraph(line, in_a_loop))
+
+        # We mustn't forget to give our bus line a name
+        self.line_id = lines[0]
+        return stop_of_graph_list
+
+# Tests
+a = LinkedStops()
+b = 8
---
+++ routes/graph/StopOfGraph.py
@@ -0,0 +1,34 @@
+class StopOfGraph:
+    """this class is here to convert the lines of the file descibing the line shape.
+    it will contain informations about the links that the stop should have
+    it's just an object to parse a line of line.txt"""
+
+    def __init__(self, line, in_a_loop=False):
+        self.name = "undefined name"
+        self.in_a_loop = in_a_loop
+
+        # Tell if we have to put a link toward the previous stop in the file
+        self.link_up = True
+
+        # Tell if we have to put a link toward the next spot in the file
+        self.link_down = True
+        self.init_from_line(line)
+
+    def init_from_line(self, line):
+        # Initialise values from the line
+
+        splited_line = line.split('\t')
+        splited_line = [x for x in splited_line if x != '']
+
+        # We get the name
+        self.name = splited_line[0]
+
+        # Here we get the directions the bus can go
+        # No number means both directions are possible
+        try:
+            if splited_line[1] == '1':
+                self.link_up = False
+            elif splited_line[1] == '2':
+                self.link_down = False
+        except:
+            print("RAS")
