db5a234df3cbb2a4585edf7c450e2d06a2f6515b
gabrieldemarmiesse <gabrieldemarmiesse@hotmail.fr>
2016-02-03 02:28:42 +0100

    The recording function is done. The comparator will be called as "with Comparator() as comparator:" so that it'll free memory at the end of the context lifecycle.

--- Comparator.py
+++ Comparator.py
@@ -2,6 +2,14 @@ from difflib import SequenceMatcher
 
 
 class Comparator:
+    """ This class is here to compare two stops and tell if they're the same.
+        An example is "Chap. Combes" and "Chapelle des Combes"
+        The main deciding factor is SequenceMatcher.
+        But after that, the user is always asked for his decision.
+        Results of user's feedback is stored in files, so that the program won't ask him again.
+        list_of_list_of_identical_stops contain what is in the file same_stops.txt
+        first_list_differentiation, second_list_differentiation come from different_stops.txt
+    """
 
     def __self__(self, first_list_differentiation, second_list_differentiation,
                  list_of_list_of_identical_stops, threshold):
@@ -23,11 +31,9 @@ class Comparator:
         if p > self.threshold:
 
             # We need to check if the names were in the files
-            result_files_search = self.compare_from_files(first_stop_name, second_stop_name)
-            if result_files_search:
-                return True
-            elif not result_files_search:
-                return False
+            found, result_files_search = self.compare_from_files(first_stop_name, second_stop_name)
+            if found:
+                return result_files_search
             else:
 
                 # We ask for user's feedback
@@ -45,7 +51,57 @@ class Comparator:
             return False
 
     def compare_from_files(self, first_stop_name, second_stop_name):
-        pass
+        """
+        This function will check is this query has already be perfomed
+        :param first_stop_name: The name of the first stop to lookup
+        :param second_stop_name: The name of the second node to lookup
+        :return: The first variable is if the query had already been made,
+          The second one is if it's really the same stop (None if we don't know from previous experiences)
+        """
+
+        # Here we compare with the stops in the differencition file
+        for i, stop_name in enumerate(self.first_list_differentiation):
+            if first_stop_name == stop_name:
+                if second_stop_name == self.second_list_differentiation[i]:
+                    return True, False
+            elif second_stop_name == stop_name:
+                if first_stop_name == self.second_list_differentiation[i]:
+                    return True, False
+
+        # Now we look into the sames stops that have different names
+        for equality in self.list_of_list_of_identical_stops:
+            found_first = False
+            found_second = False
+            for stop_name in equality:
+                if stop_name == first_stop_name:
+                    found_first = True
+                if stop_name == second_stop_name:
+                    found_second = True
+            if found_first and found_second:
+                return True, True
+
+        return False, None
 
     def store_relation(self, matching, first_stop_name, second_stop_name):
+        if matching:
+            self.store_different_stops(first_stop_name, second_stop_name)
+        else:
+            self.store_same_stops(first_stop_name, second_stop_name)
+
+    def store_different_stops(self, first_stop_name, second_stop_name):
+
+            pass
+
+    def store_same_stops(self, first_stop_name, second_stop_name):
         pass
+
+    def __exit__(self, *err):
+        self.write_to_disk()
+
+    def write_to_disk(self):
+        # Here we write the data from the object in files so that it can be destroyed
+        pass
+
+    def __enter__(self):
+        print('creating comparator')
+        return self  # this is bound to the `as` part
--- test.py
+++
@@ -1,9 +0,0 @@
-import io
-
-with io.open("lol.txt", "r", encoding="utf-8") as f:
-    bla = f.readline()
-
-
-file = io.open("lolfjr.txt", "w", encoding="utf-8")
-file.write(bla)
-file.close()
